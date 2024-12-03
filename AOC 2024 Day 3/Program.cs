19258ad2db53a4757d4c728b0bf1361d362c071f
Ikbal Yaman <m.ikbal.yaman@gmail.com>
2024-12-03 16:31:10 +0300

    Day 3 completed

---
+++ AOC 2024 Day 3/Program.cs
@@ -0,0 +1,59 @@
+﻿using AOC_2024_Day_3.Properties;
+using System.Text.RegularExpressions;
+
+var data = Resources.ResourceManager.GetString("Data")!;
+
+Part1(data);
+Part2(data);
+
+static void Part1(string data)
+{
+    long sum = 0;
+    for (var match = Part1Regex().Match(data); match.Success; match = match.NextMatch())
+    {
+        var variables = match.ValueSpan[4..^1];
+        var commaIdx = variables.IndexOf(',');
+        var numA = int.Parse(variables[..(commaIdx)]);
+        var numB = int.Parse(variables[(commaIdx + 1)..]);
+        sum += numA * numB;
+    }
+    Console.WriteLine(sum);
+}
+
+static void Part2(string data)
+{
+    bool state = true;
+    long sum = 0;
+    for (var match = Part2Regex().Match(data); match.Success; match = match.NextMatch())
+    {
+        var value = match.Value;
+
+        if (value is "do()")
+        {
+            state = true;
+            continue;
+        }
+        if(value is "don't()")
+            state = false;
+
+        if (!state)
+            continue;
+
+        var variables = match.ValueSpan[4..^1]; 
+        var commaIdx = variables.IndexOf(',');
+
+        var numA = int.Parse(variables[..(commaIdx)]);
+        var numB = int.Parse(variables[(commaIdx + 1)..]);
+        sum += numA * numB;
+    }
+    Console.WriteLine(sum);
+}
+
+partial class Program
+{
+    [GeneratedRegex("mul\\(\\d{1,3},\\d{1,3}\\)")]
+    private static partial Regex Part1Regex();
+
+    [GeneratedRegex("(mul\\(\\d{1,3},\\d{1,3}\\))|(do\\(\\))|(don't\\(\\))")]
+    private static partial Regex Part2Regex();
+}
